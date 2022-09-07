b809d45c483e8dc332d45e1f82c784530b790d2e
Ilyas Aleskerov <i.aleskerov@yandex.ru>
2022-09-08 00:17:36 +0300

    fix: fixed response code in exceptions to default

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmValidationException.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class FilmValidationException extends RuntimeException{
     public FilmValidationException(String message) {
         super(message);
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
@@ -1,9 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserValidationException extends RuntimeException {
     public UserValidationException(String message) {
         super(message);
