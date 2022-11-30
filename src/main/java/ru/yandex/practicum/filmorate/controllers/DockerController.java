3185e05595336b066eba056a6df7794f3fe1631d
Ilyas Aleskerov <i.aleskerov@yandex.ru>
2022-11-30 15:02:09 +0300

    added controller for main page

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/DockerController.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String helloDocker() {
+        return "Hello from Docker";
+    }
+}
